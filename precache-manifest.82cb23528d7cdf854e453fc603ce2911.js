59c861cab9df5ea98f76ad35b087c98ed3fd33c9
Spencer Jack <spencer.jack@alaskaair.com>
2019-03-06 16:46:38 -0800

    Updates

---
+++ precache-manifest.82cb23528d7cdf854e453fc603ce2911.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b54391c3182fb52b2c6",
+    "url": "/donut_heaven/static/js/runtime~main.6b54391c.js"
+  },
+  {
+    "revision": "542516466bbbd228f3d4",
+    "url": "/donut_heaven/static/js/main.54251646.chunk.js"
+  },
+  {
+    "revision": "8593d4d2833458843ca0",
+    "url": "/donut_heaven/static/js/2.8593d4d2.chunk.js"
+  },
+  {
+    "revision": "542516466bbbd228f3d4",
+    "url": "/donut_heaven/static/css/main.42fcb8f8.chunk.css"
+  },
+  {
+    "revision": "06d2608e7e5479137636f24986f5241d",
+    "url": "/donut_heaven/index.html"
+  }
+];
